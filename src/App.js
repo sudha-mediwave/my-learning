6dadc029b9c157874fcc8b25ac8c65fb982e94f6
sudha-mediwave <sudha@mindwaveventures.com>
2023-05-17 12:20:39 +0530

    add basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const InputList = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [inputList, setInputList] = useState([]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAdd = () => {
+    if (inputValue.trim() !== '') {
+      const uniqueId = Math.floor(1000 + Math.random() * 9000);
+      const addObj = {
+        id: uniqueId,
+        name: inputValue,
+      };
+      console.log('addObj :>>', addObj);
+      setInputList([...inputList, addObj]);
+      setInputValue('');
+    }
+  };
+
+  const handleEdit = (index) => {
+    const editedValue = prompt('Enter the new value:');
+    if (editedValue !== null && editedValue.trim() !== '') {
+      const updatedList = [...inputList];
+      updatedList[index] = editedValue;
+      setInputList(updatedList);
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedList = [...inputList];
+    updatedList.splice(index, 1);
+    setInputList(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={handleChange}
+        placeholder='Enter a value'
+      />
+      <button onClick={handleAdd}>Add</button>
+      <pre>{JSON.stringify(inputList, null, 4)}</pre>
+      {/* <ul>
+        {inputList.map((value, index) => (
+          <li key={index}>
+            {value}
+            <button onClick={() => handleEdit(index)}>Edit</button>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul> */}
     </div>
   );
-}
+};
 
-export default App;
+export default InputList;
